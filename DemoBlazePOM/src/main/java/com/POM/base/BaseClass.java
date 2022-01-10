f06c4d0b4b94531048497f904165867edc38208e
AleeshaCAsharaf <91299776+AleeshaCAsharaf@users.noreply.github.com>
2022-01-10 09:42:44 +0530

    Update BaseClass.java

--- DemoBlazePOM/src/main/java/com/POM/base/BaseClass.java
+++ DemoBlazePOM/src/main/java/com/POM/base/BaseClass.java
@@ -1,5 +1,38 @@
 package com.POM.base;
 
-public class BaseClass {
+import org.openqa.selenium.WebDriver;
 
-}
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import com.POM.pages.HomePage;
+
+
+public class BaseClass {
+	
+
+
+	public HomePage homePage;	
+	public WebDriver driver;
+	
+	
+	@BeforeMethod
+	public  void  Setup() {
+		System.setProperty("webdriver.chrome.driver", 
+			     "C:\\Users\\Admin\\Downloads\\chromedriver_win32 (3)\\chromedriver.exe");
+					WebDriver driver = new ChromeDriver();
+					driver.get("https://www.demoblaze.com/");		
+					driver.manage().window().maximize();
+					homePage = new HomePage();
+					
+	}
+	
+	@AfterMethod
+	public void tearDown()
+	{
+		driver.close();
+	}
+	
+	}
+
