cac47f8e93f2ade2b3c03d22865547f42b91f1cc
AleeshaCAsharaf <91299776+AleeshaCAsharaf@users.noreply.github.com>
2022-01-10 09:46:14 +0530

    Update HomePageTest.java

--- DemoBlazePOM/src/test/java/com/POM/testcases/HomePageTest.java
+++ DemoBlazePOM/src/test/java/com/POM/testcases/HomePageTest.java
@@ -1,5 +1,26 @@
 package com.POM.testcases;
 
-public class HomePageTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.POM.base.BaseClass;
+
+
+import com.POM.pages.ProductPage;
+
+public class HomePageTest extends BaseClass {
+	
+	
+	ProductPage productPage;
+	
+	@Test
+	public void homePageTest() {
+		productPage = homePage.products();
+		String expectedURL ="https://www.demoblaze.com/#";
+		String actualURL = driver.getCurrentUrl();
+		Assert.assertEquals(actualURL, expectedURL);
+		
+	}
 
 }
+
