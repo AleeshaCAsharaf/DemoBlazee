f5373dcc447608b3c5ec249948df24e07e9d6758
AleeshaCAsharaf <91299776+AleeshaCAsharaf@users.noreply.github.com>
2022-01-10 09:46:55 +0530

    Update ProductPageTest.java

--- DemoBlazePOM/src/test/java/com/POM/testcases/ProductPageTest.java
+++ DemoBlazePOM/src/test/java/com/POM/testcases/ProductPageTest.java
@@ -1,5 +1,23 @@
 package com.POM.testcases;
 
-public class ProductPageTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.POM.base.BaseClass;
+import com.POM.pages.HomePage;
+import com.POM.pages.ProductPage;
+
+public class ProductPageTest extends BaseClass {
+	ProductPage productPage;
+	HomePage homePage;
+	
+	@Test
+	public void productsResult() {
+		productPage = homePage.products();
+		productPage.clickOnProduct();
+		String expectedURL = "https://www.demoblaze.com/prod.html?idp_=8";
+		String actualURL = driver.getCurrentUrl();
+		Assert.assertEquals(actualURL, expectedURL);
+	}
 
 }
