54cfc70f8a4120502c6abfdcaaf65d53a10bed73
AleeshaCAsharaf <91299776+AleeshaCAsharaf@users.noreply.github.com>
2022-01-10 09:44:33 +0530

    Update ProductDisplayPage.java

--- DemoBlazePOM/src/main/java/com/POM/pages/ProductDisplayPage.java
+++ DemoBlazePOM/src/main/java/com/POM/pages/ProductDisplayPage.java
@@ -1,5 +1,23 @@
 package com.POM.pages;
 
-public class ProductDisplayPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.POM.base.BaseClass;
+
+
+public class ProductDisplayPage extends BaseClass{
+	@FindBy(xpath="//a[@onClick='addToCart(8)']")
+	WebElement ProductCart;
+	
+	public ProductDisplayPage() {
+		PageFactory.initElements(driver, this);
+		
+	}
+	
+	public void AddToCart() {
+		ProductCart.click();
+	}
 
 }
