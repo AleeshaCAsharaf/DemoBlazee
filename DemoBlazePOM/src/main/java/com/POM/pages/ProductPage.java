4a36f533c768cf218bc57094d17f59fd47d23b4d
AleeshaCAsharaf <91299776+AleeshaCAsharaf@users.noreply.github.com>
2022-01-10 09:45:11 +0530

    Update ProductPage.java

--- DemoBlazePOM/src/main/java/com/POM/pages/ProductPage.java
+++ DemoBlazePOM/src/main/java/com/POM/pages/ProductPage.java
@@ -1,5 +1,24 @@
 package com.POM.pages;
 
-public class ProductPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import com.POM.base.BaseClass;
+
+
+public class ProductPage extends BaseClass {
+	@FindBy(xpath="//a[@href='prod.html?idp_=8']//img[@class='card-img-top img-fluid']")
+	WebElement product;
+	
+	public ProductPage() {
+		PageFactory.initElements(driver, this);
+		
+	}
+	
+	public ProductDisplayPage clickOnProduct() {
+		product.click();
+		return new ProductDisplayPage();
+	}
 }
+
