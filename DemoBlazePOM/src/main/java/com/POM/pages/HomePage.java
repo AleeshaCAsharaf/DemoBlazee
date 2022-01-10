00a5fa14a34e55bb098714adc9353f045d5a1418
AleeshaCAsharaf <91299776+AleeshaCAsharaf@users.noreply.github.com>
2022-01-10 09:43:42 +0530

    Update HomePage.java

--- DemoBlazePOM/src/main/java/com/POM/pages/HomePage.java
+++ DemoBlazePOM/src/main/java/com/POM/pages/HomePage.java
@@ -1,5 +1,25 @@
 package com.POM.pages;
 
-public class HomePage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.POM.base.BaseClass;
+
+
+
+public class HomePage extends BaseClass {
+
+	@FindBy(xpath="//div[@class='list-group']//a[text()='Laptops']")
+	WebElement category;
+	
+public HomePage() {
+	PageFactory.initElements(driver, this);
+	
+}
+public ProductPage products() {
+	category.click();
+	return new ProductPage();
+}
 
 }
